6fe3daa881c3a131dab03706405645d3a789905c
Marius Guede <marius.guede@sleeksys.com>
2025-06-19 09:15:37 +0200

    initial commit

---
+++ src/main/java/com/sleeksys/logger/LogManagementWsApplication.java
@@ -0,0 +1,13 @@
+package com.sleeksys.logger;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class LogManagementWsApplication {
+
+    public static void main(String[] args) {
+        run(LogManagementWsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sleeksys/logger/LogManagementWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleeksys.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogManagementWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
